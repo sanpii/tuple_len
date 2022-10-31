ea2cf65cc33ee949df7cee118fb56edd9742ca94
Sanpi <sanpi@homecomputing.fr>
2022-10-31 21:27:34 +0100

    Supports empty tupple

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ macro_rules! tuple_len {
     ( ($($a:expr),+) ) => { $crate::tuple_len!(1, $($a,)+) };
     ( $len:expr, $a:expr, $($rest_a:expr,)+ ) => { $crate::tuple_len!($len + 1, $($rest_a,)+) };
     ( $len:expr, $a:expr, ) => { $len };
+    ( () ) => { 0 };
 }
 
 #[cfg(test)]
@@ -14,6 +15,7 @@ mod tests {
     fn tuple_len() {
         let _x: u8;
 
+        assert_eq!(tuple_len!(()), 0);
         assert_eq!(tuple_len!((1)), 1);
         assert_eq!(tuple_len!(((1, 1))), 1);
         assert_eq!(tuple_len!((_x, _x)), 2);
