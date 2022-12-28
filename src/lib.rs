7af7c199428d07dba4a35f76c88c398cd1902ac9
Sanpi <sanpi@homecomputing.fr>
2022-12-28 12:27:40 +0100

    Adds TupleLen trait

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,43 @@ macro_rules! tuple_len {
     ( () ) => { 0usize };
 }
 
+#[allow(clippy::len_without_is_empty)]
+pub trait TupleLen {
+    fn len(&self) -> usize;
+}
+
+macro_rules! count {
+    () => (0usize);
+    ( $x:tt $($xs:tt)* ) => (1usize + count!($($xs)*));
+}
+
+macro_rules! tuple_impl {
+    ($( $T:ident ),*) => {
+        impl<$( $T, )*> TupleLen for ($( $T, )*) {
+            fn len(&self) -> usize {
+                count!($( $T )*)
+            }
+        }
+    }
+}
+
+tuple_impl!();
+tuple_impl!(A, B);
+tuple_impl!(A, B, C);
+tuple_impl!(A, B, C, D);
+tuple_impl!(A, B, C, D, E);
+tuple_impl!(A, B, C, D, E, F);
+tuple_impl!(A, B, C, D, E, F, G);
+tuple_impl!(A, B, C, D, E, F, G, H);
+tuple_impl!(A, B, C, D, E, F, G, H, I);
+tuple_impl!(A, B, C, D, E, F, G, H, I, J);
+tuple_impl!(A, B, C, D, E, F, G, H, I, J, K);
+tuple_impl!(A, B, C, D, E, F, G, H, I, J, K, L);
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn tuple_len() {
+    fn r#macro() {
         let _x: u8;
 
         assert_eq!(tuple_len!(()), 0);
@@ -21,5 +54,17 @@ mod tests {
         assert_eq!(tuple_len!((_x, _x)), 2);
         assert_eq!(tuple_len!((_x, 1, _x)), 3);
         assert_eq!(tuple_len!((_x, _x, Some("foo"), || {})), 4);
+    }
+
+    #[test]
+    fn r#trait() {
+        use crate::TupleLen;
+
+        let _x: u8 = 0;
+
+        assert_eq!(().len(), 0);
+        assert_eq!((_x, _x).len(), 2);
+        assert_eq!((_x, 1, _x).len(), 3);
+        assert_eq!((_x, _x, Some("foo"), || {}).len(), 4);
     }
 }
